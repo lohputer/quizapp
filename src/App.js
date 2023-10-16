70df876984a7733a7d0ecf4ca3b1251dad7df166
Lyndon Loh <108809431+lohputer@users.noreply.github.com>
2023-10-16 17:15:42 +0800

    test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/scss/bootstrap.scss';
+import "/node_modules/bootstrap/dist/css/bootstrap.css";
+function App() {
+  return (
+    <>
+      <div className="row col-10 m-auto p-4 text-center bg-info">
+        <h1 className="text-primary">A Fun Quiz</h1>
+        <p>Hi yalls!</p>
+      </div>
+    </>
+  );
+}
+export default App;
