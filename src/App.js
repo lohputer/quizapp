dfbeeaab0cc8b4a74888c13a2ce35a06f00a4dd6
Lyndon Loh <108809431+lohputer@users.noreply.github.com>
2023-10-16 22:37:23 +0800

    okay wtf

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/scss/bootstrap.scss';
 import "/node_modules/bootstrap/dist/css/bootstrap.css";
+import './custom.scss';
 function App() {
   return (
     <>
-      <div className="row col-10 m-auto p-4 text-center bg-info">
+      <div className="row col-10 m-auto p-4 text-center">
         <h1 className="text-primary">A Fun Quiz</h1>
-        <p>Hi yalls!</p>
+        <p>Hi yalls! I quite frankly have lost my coding skills after 2 months and need to get back at it.</p>
+        <p>To celebrate, I made a 50 MCQ quiz with random stuff. It's 30 minutes limit. For fun. Good luck :D</p>
       </div>
     </>
   );
