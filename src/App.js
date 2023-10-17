52fe5321c11b8934c42a57045ae8b222aa5e95ca
Lyndon Loh <108809431+lohputer@users.noreply.github.com>
2023-10-18 00:09:44 +0800

    test

--- src/App.js
+++ src/App.js
@@ -8,56 +8,83 @@ export default function App() {
   const [ content, setContent ] = useState("");
   const [ question, setQn ] = useState(-1);
   const [ seconds, setTime ] = useState(1800);
+  const [ intervalId, setIntervalId ] = useState(null);
+  const [ answers, setAnswers ] = useState(Array(50).fill(NaN));
+  function answer(x) {
+    setAnswers((list) => {
+      const updatedList = [...list];
+      updatedList[question] = x;
+      return updatedList;
+    });
+  }
   useEffect(()=>{
     if (question !== -1) {
-      var intervalId = setInterval(function() {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+      var newIntervalId = setInterval(function() {
         setTime(prevTime => prevTime - 1);
         clearInterval(intervalId);
       }, 1000);
+      setIntervalId(newIntervalId);
       (async function() {
         try {
           const response = await fetch(`${process.env.PUBLIC_URL}/questions.txt`);
           const data = await response.text(); 
-          const questions = data.split("-end-\r\n");
-          console.log(questions);
-          document.getElementById("quiz").style.transform = "rotate(360deg)";
+          const questions = data.split("-end-\n");
           setTimeout(()=>{
+            console.log(question, answers[question]);
             setContent(
               <>
                 <h1 className="text-primary">{questions[question].split("\n")[0]}</h1>
-                <div className="form-check p-4 text-left">
-                  <label className="form-check-label">
-                    <input type="radio" className="form-check-input" name={question} value="1" /> {questions[question].split("\n")[1]}
+                <div className="form-check p-4">
+                  <label className="form-check-label" htmlFor={"q"+(question+1)} >
+                    <input type="radio" className="form-check-input" name={"q"+(question+1)} onChange={()=>answer(1)} checked={answers[question] === 1 ? true : false} /> {questions[question].split("\n")[1]}
                   </label>
                   <br></br>
-                  <label className="form-check-label">
-                    <input type="radio" className="form-check-input" name={question} value="2" /> {questions[question].split("\n")[2]}
+                  <label className="form-check-label" htmlFor={"q"+(question+1)} >
+                    <input type="radio" className="form-check-input" name={"q"+(question+1)} onChange={()=>answer(2)} checked={answers[question] === 2 ? true : false}  /> {questions[question].split("\n")[2]}
                   </label>
                   <br></br>
-                  <label className="form-check-label">
-                    <input type="radio" className="form-check-input" name={question} value="3" /> {questions[question].split("\n")[3]}
+                  <label className="form-check-label" htmlFor={"q"+(question+1)} >
+                    <input type="radio" className="form-check-input" name={"q"+(question+1)} onChange={()=>answer(3)} checked={answers[question] === 3 ? true : false}  /> {questions[question].split("\n")[3]}
                   </label>
                   <br></br>
-                  <label className="form-check-label">
-                    <input type="radio" className="form-check-input" name={question} value="4" /> {questions[question].split("\n")[4]}
+                  <label className="form-check-label" htmlFor={"q"+question} >
+                    <input type="radio" className="form-check-input" name={"q"+(question+1)} onChange={()=>answer(4)} checked={answers[question] === 4 ? true : false} /> {questions[question].split("\n")[4]}
                   </label>
                 </div>
-                  {question !== 0 && <button className="btn btn-primary p-2" onClick={() => setQn(x => x-1)}>Previous Question</button>}
-                  <p>{Math.floor(seconds / 3600) === 0 ? "00": Math.floor(seconds / 3600)}:{Math.floor((seconds - (Math.floor(seconds / 3600) * 3600))/60) === 0 ? "00" : Math.floor((seconds - (Math.floor(seconds / 3600) * 3600))/60)}:{seconds-(Math.floor(seconds / 3600) * 3600)-(Math.floor((seconds - (Math.floor(seconds / 3600) * 3600))/60)*60) === 0 ? "00" : seconds-(Math.floor(seconds / 3600) * 3600)-(Math.floor((seconds - (Math.floor(seconds / 3600) * 3600))/60)*60)} </p>
-                  <button className="btn btn-primary p-2" onClick={() => setQn(x => x+1)}>Next Question</button>
+                <p className={question !== 0 && "text-center"}>
+                  {String(Math.floor(seconds / 3600)).padStart(2, '0')}:
+                  {String(Math.floor((seconds - Math.floor(seconds / 3600) * 3600) / 60)).padStart(2, '0')}:
+                  {String(seconds - Math.floor(seconds / 3600) * 3600 - Math.floor((seconds - Math.floor(seconds / 3600) * 3600) / 60) * 60).padStart(2, '0')}
+                </p>
+                <div className="row d-flex">
+                  {question !== 0 && <button className="btn btn-primary col-5 m-auto" onClick={() => setQn(x => x-1)}>Previous</button>}
+                  <button className={question !== 0 ? "btn btn-primary col-5 m-auto" : "btn btn-primary"} onClick={() => setQn(x => x+1)}>Next</button>
+                </div>
               </>
             );
           }, 50);
-          console.log(question, questions);
         } catch (error) {
           console.log(error);
         }
-
       })();
-
     }
-    
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
   }, [question, seconds]);
+  useEffect(()=>{
+    if (question !== -1) {
+      document.getElementById("quiz").style.transform = "rotate(360deg)";
+      setTimeout(()=>{
+        document.getElementById("quiz").style.transform = "none";
+      }, 100);
+    }
+  }, [question]);
   return (
     <div className="App vh-100 vw-100 d-flex align-items-center justify-content-center">
       <div id="quiz" className="container col-8 m-auto p-4 bg-info text-center rounded shadow">
